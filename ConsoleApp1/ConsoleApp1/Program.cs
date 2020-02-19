d7de611537c0dc1962fb253c39d371b38899e57b
L <L@LAPTOP-CID5FE6V>
2020-02-19 20:56:47 +0800

    dev8

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,8 @@ namespace ConsoleApp1
             Console.WriteLine("four");
             Console.WriteLine("five");
             Console.WriteLine("6");
+            Console.WriteLine("7777");
+            Console.WriteLine("888");
         }
     }
 }
