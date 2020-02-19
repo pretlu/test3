a6aee54946eb75c1c3c54ab4f025c59c0d0e4367
L <L@LAPTOP-CID5FE6V>
2020-02-19 17:16:17 +0800

    修改hello world

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,10 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("2222fix");
-            Console.WriteLine("3333");
-            Console.WriteLine("444");
+            Console.WriteLine("Hello World! 我从first分支中修改了这个");
         }
     }
 }
