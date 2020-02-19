e528ff223e1322160026d0dceb2f8037b5b4892b
L <L@LAPTOP-CID5FE6V>
2020-02-19 16:33:03 +0800

    只提交暂存更改33

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,8 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("2222fix");
+            Console.WriteLine("3333");
+
         }
     }
 }
