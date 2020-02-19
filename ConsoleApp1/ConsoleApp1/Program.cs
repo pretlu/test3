0a77d4f3201354233834ee16c5017665b11d71f9
L <L@LAPTOP-CID5FE6V>
2020-02-19 21:23:08 +0800

    clear

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,19 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("one");
-            Console.WriteLine("two");
-            Console.WriteLine("three");
-            Console.WriteLine("four");
-            Console.WriteLine("five");
-            Console.WriteLine("6");
-            Console.WriteLine("7777");
-            Console.WriteLine("888");
-<<<<<<< HEAD
-            Console.WriteLine("9");
-            Console.WriteLine("10");
-
->>>>>>> add45
+     
 
         }
     }
